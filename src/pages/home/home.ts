6dbdfb35fa417c1405b6224ad7209a4936316327
cameronadams777 <cameronadams777@gmail.com>
2017-12-10 01:57:58 -0600

    add provider for thebluealliance api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+import { TheBlueAllianceServiceProvider } from '../../providers/the-blue-alliance-service/the-blue-alliance-service'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,9 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http, 
+    public theBlueAllianceAPI: TheBlueAllianceServiceProvider) {
 
   }
-
+  
 }
