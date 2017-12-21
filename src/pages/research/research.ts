22374dea8b9f45452d15c2cd4eca3a4e637d0183
Alejandro <Alejandro.cantu1018@gmail.com>
2017-12-20 20:37:41 -0600

    WIP added tabs to home page

---
+++ src/pages/research/research.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-research',
+  templateUrl: 'research.html'
+})
+export class ResearchPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+}
+
---
+++ src/pages/scouting/scouting.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-scouting',
+  templateUrl: 'scouting.html'
+})
+export class ScoutingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+}
+
